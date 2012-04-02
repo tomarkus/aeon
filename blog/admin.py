8cc4d9333c2356ed6be7fb93b9934702bbb18973
xando <sebastian.pawlus@gmail.com>
2012-04-02 11:28:08 +0100

    Admin tweaks

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from blog.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'content', 'created', 'modified')
+    readonly_fields = ('created', 'modified')
+    list_display = ('id', 'title', 'created', 'modified')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+
+admin.site.register(Post, PostAdmin)
